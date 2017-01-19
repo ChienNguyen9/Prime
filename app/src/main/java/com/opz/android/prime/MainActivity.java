a9e02355836488fa29317bea00a2bc0d84998537
Chien Nguyen <cmnguyen999@yahoo.com>
2017-01-18 20:40:05 -0600

    Checks to see if number is prime[works]

--- app/src/main/java/com/opz/android/prime/MainActivity.java
+++ app/src/main/java/com/opz/android/prime/MainActivity.java
@@ -7,14 +7,59 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Prime mPrime;
+    private EditText mPrimeInput;
+    private Button mCheckPrimeButton;
+
+    private void update(){
+        if (mPrime.getStoreNumber() == "1") {
+            Toast.makeText(this, R.string.prime_number, Toast.LENGTH_LONG).show();
+        }else{
+            if (mPrime.isCheckPrime()) {
+                Toast.makeText(this, R.string.prime_number, Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, R.string.not_prime_number, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mPrime = new Prime();
 
+        mPrimeInput = (EditText) findViewById(R.id.prime_editText);
+        mCheckPrimeButton = (Button) findViewById(R.id.check_prime_button);
+
+        // Whenever text changes the new string is stored
+        mPrimeInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mPrime.setStoreNumber(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        // Button checks if it is a prime or not
+        mCheckPrimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                update();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/opz/android/prime/Prime.java
@@ -0,0 +1,27 @@
+package com.opz.android.prime;
+
+/**
+ * Created by zCatchMee on 1/18/17.
+ */
+
+public class Prime {
+    private String mStoreNumber;
+
+    public String getStoreNumber() {
+        return mStoreNumber;
+    }
+
+    public void setStoreNumber(String storeNumber) {
+        mStoreNumber = storeNumber;
+    }
+
+    public boolean isCheckPrime() {
+        int number = Integer.parseInt(mStoreNumber.toString());
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
