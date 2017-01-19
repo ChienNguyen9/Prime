8a8c1f702cb2a79b6a8cebeb43e98b09de6b23d4
Chien Nguyen <cmnguyen999@yahoo.com>
2017-01-18 18:03:15 -0600

    Layout with Button/EditText

--- app/src/main/java/com/opz/android/prime/MainActivity.java
+++ app/src/main/java/com/opz/android/prime/MainActivity.java
@@ -2,6 +2,11 @@ package com.opz.android.prime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
